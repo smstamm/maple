65fcfcd9d28a5bc8dd5d35ab2908108e9bf0d337
Kate <kateharkness@outlook.com>
2015-09-11 23:03:15 -0600

    adds hometown and avatar to the code

--- app.js
+++ app.js
@@ -3,19 +3,29 @@ var app = angular.module('app', []);
 app.controller('home-ctrl', ['$scope', function($scope){
 	$scope.teamMembers = [
 		{
-			name: "Bobby"
+			name: "Bobby",
+			hometown: "South Jordan",
+			avatar: "https://avatars1.githubusercontent.com/u/13859571?v=3&s=400"
 		},
 		{
-			name: "Steph"
+			name: "Steph",
+			hometown: "Sugarhouse",
+			avatar: "https://avatars1.githubusercontent.com/u/12092523?v=3&s=400"
 		},
 		{
-			name: "Kate"
+			name: "Kate",
+			hometown: "Lehi",
+			avatar: "https://avatars0.githubusercontent.com/u/10120733?v=3&s=400"
 		},
 		{
-			name: "Andre"
+			name: "Andre",
+			hometown: "Sandy",
+			avatar: "https://avatars2.githubusercontent.com/u/4837992?v=3&s=400"
 		},
 		{
-			name: "Dan"
+			name: "Dan",
+			hometown: "Salt Lake City",
+			avatar: "https://avatars1.githubusercontent.com/u/3272723?v=3&s=400"
 		}
 	]
 }]);
